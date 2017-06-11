6920aff4a24b89c6a77700a8c662275df4e56cda
Emerson-Wenzel <emerson.wenzel@tufts.edu>
2017-06-11 15:51:36 -0400

    Initial buyMain.js

--- buyMain.js
+++ buyMain.js
@@ -1,10 +1,67 @@
 console.log("start of buyMain.js");
 
-function sendSellData(){
+$(document).ready(function(){
+    console.log("Beginning of sendBuyData()")
     
+    //Variable to hold request
+    var request;
+
+    // bind to the submit event in our form
+
+    $("#buyForm").submit(function(event){
     
+        //Abort any pending request
+        if (request) {
+            console.log("aborting request");
+            request.abort()
+        
+        }
+
+        // setup some local variables
+        var $form = $(this);
+        console.log("set up local variables");
     
+        //Select and cache all fields
+        var $inputs = $form.find("input, select, button, textarea");
     
+        //Serialize the data in the form
+        var serializedData = $form.serialize();
+        console.log(serializedData);
+
+            //Disable inputs while Ajax request is occuring
+    $inputs.prop("disabled", true);
+
+    console.log("before sending request");
+    //Send the request
+    request = $.ajax({                      
+        url:"https://script.google.com/macros/s/AKfycbydLfnoSs0icYiMoFaC1LkjDUJM_M9IVU9AWNKtFuomRxXBxWU/exec",
+        type: "post",
+        data: serializedData
+    });
+        
+        //Callback handler that will be called on success
+    request.done(function(response, textStatus, jqXHR){
+        //Log message to console
+        console.log("buyMain AJAX worked!");
+        console.log(response)
+        console.log(response[1])
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
+
+    //Callback handler that will be called on failure
+    request.fail(function(jqXHR, textStatus, errorThrown){
+        console.error("The following error occured: " + textStatus, erorrThrown);
+    });
+
+    //handler that will always be called
+    request.always(function(){
+        //Enable inputs again
+        $inputs.prop("disabled", false);
+    });
     
-    
-}
+    //Prevent default posting of form
+    event.preventDefault();
+    console.log("sendData")
+    });
+});
